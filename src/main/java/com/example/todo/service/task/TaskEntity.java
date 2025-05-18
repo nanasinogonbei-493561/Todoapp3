00817cae66794d6a70dc9e237356f06b8f06b438
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2025-05-18 17:28:06 +0900

    TaskEntity.javaを変更しました。

--- src/main/java/com/example/todo/service/task/TaskEntity.java
+++ src/main/java/com/example/todo/service/task/TaskEntity.java
@@ -1,9 +1,16 @@
 package com.example.todo.service.task;
 
+
 public record TaskEntity(
         Long id,
         String summary,
         String description,
-        TaskStatus status
+        TaskStatus status,
+        Long categoryId
 ) {
+
+//     既存のコードとの互換性のためのファクトリメソッド
+    public static TaskEntity of(Long id, String summary, String description, TaskStatus status){
+        return new TaskEntity(id, summary, description, status, null);
+    }
 }
