2b9a3c5fd185889de3df8fd81b32700d80b230de
菅原　祥宏 <sugawara@sugawarasachihiroshinoMacBook-Air.local>
2025-06-16 16:08:59 +0900

    application.propertiesとdata.sqlとschema.sqlの変更をしました。

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,8 @@
-INSERT INTO tasks (summary, description, status) VALUES ('Spring Boot を学ぶ', 'TODO アプリを作る', 'DONE');
-INSERT INTO tasks (summary, description, status) VALUES ('Spring Security を学ぶ', 'ログイン機能を作る', 'TODO');
+INSERT INTO categories (name, description) VALUES ('勉強', '勉強について');
+INSERT INTO categories (name, description) VALUES ('運動', '運動について');
+INSERT INTO categories (name, description) VALUES ('買い物', '買い物について');
+INSERT INTO categories (name, description) VALUES ('家事', '家事について');
+
+
+INSERT INTO tasks (category_id, summary, description, status) VALUES (1, 'Spring Boot を学ぶ', 'TODO アプリを作る', 'DONE');
+INSERT INTO tasks (category_id, summary, description, status) VALUES (1, 'Spring Security を学ぶ', 'ログイン機能を作る', 'TODO');
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,27 @@
-CREATE TABLE tasks
-(
-  id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- H2では FOREIGN_KEY_CHECKS を使わない
+
+
+-- 既存のテーブルがあれば削除
+DROP TABLE IF EXISTS tasks;
+DROP TABLE IF EXISTS categories;
+
+
+-- categories テーブル作成
+CREATE TABLE categories (
+ id BIGINT PRIMARY KEY AUTO_INCREMENT,
+ name VARCHAR(256) NOT NULL,
+ description TEXT
+);
+
+
+-- tasks テーブル作成
+CREATE TABLE tasks (
+  id BIGINT PRIMARY KEY AUTO_INCREMENT,
+  category_id BIGINT NOT NULL,
   summary VARCHAR(256) NOT NULL,
   description TEXT,
-  status VARCHAR(256) NOT NULL
+  status VARCHAR(256) NOT NULL,
+  CONSTRAINT fk_category
+     FOREIGN KEY (category_id) REFERENCES categories(id)
+     ON DELETE CASCADE
 );
